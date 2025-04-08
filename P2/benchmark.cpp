f221aa964306e6c884737710377aa0c125472ad0
hokkaydo <hokkaydo@users.noreply.github.com>
2025-04-08 17:45:07 +0200

    update

--- P2/benchmark.cpp
+++ P2/benchmark.cpp
@@ -24,26 +24,26 @@ int main(int argc, char** argv) {
     }
     for (int i = 0; i < maxIterations; ++i) {
         int size = (int)pow(10, (sizeExpRangeEnd - sizeExpRangeStart) * (double)i / (double)maxIterations + sizeExpRangeStart);
-        Matrix A(size, size);
-        Matrix B(size, size);
+        // Matrix A(size, size);
+        // Matrix B(size, size);
         
         // Initialize matrices with random values
-        for (int j = 0; j < size; ++j) {
-            for (int k = 0; k < size; ++k) {
-                A.set(j, k, (double)rand() / (double)RAND_MAX);
-                B.set(j, k, (double)rand() / (double)RAND_MAX);
-            }
-        }
+        // for (int j = 0; j < size; ++j) {
+        //     for (int k = 0; k < size; ++k) {
+        //         A.set(j, k, (double)rand() / (double)RAND_MAX);
+        //         B.set(j, k, (double)rand() / (double)RAND_MAX);
+        //     }
+        // }
         
         // Create distributed matrices
-        DistributedMatrix distA(A, numProcesses);
-        DistributedMatrix distB(B, numProcesses);
+        // DistributedMatrix distA(A, numProcesses);
+        // DistributedMatrix distB(B, numProcesses);
         
         // Start timing
         double startTime = MPI_Wtime();
         
         // Perform multiplication
-        Matrix res = distA.multiplyTransposed(distB);
+        // Matrix res = distA.multiplyTransposed(distB);
         
         // End timing
         double endTime = MPI_Wtime();
